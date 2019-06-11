84195b5a89109c703b54d31b151b0ddab575a4e0
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-06-11 21:19:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,24 @@ def map_to_negativize(source_array)
   end
   return new_array
 end
+def map_to_no_change(source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array.push(source_array[index])
+  end
+  return new_array
+end
+def map_to_double(source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array.push(source_array[index] * 2)
+  end
+  return new_array
+end
+def map_to_square(source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array.push(source_array[index] ** 2)
+  end
+  return new_array
+end
